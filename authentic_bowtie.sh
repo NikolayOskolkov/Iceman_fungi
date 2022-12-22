d756718ac85167d3a471b2e86f737fa7622bb93e
Nikolay Oskolkov <49948664+NikolayOskolkov@users.noreply.github.com>
2022-12-22 16:42:17 +0100

    Add files via upload

---
+++ authentic_bowtie.sh
@@ -0,0 +1,40 @@
+#AUTHENTICATION WORKFLOW FOR BOWTIE2
+#Example command line: 
+#./authentic_bowtie.sh 96345 P17556_1021_S21_L004_R1_001.trimmed_BOWTIE2_FULL_NT.sam
+
+TAXID=$1
+SAM=$2
+
+ml bioinfo-tools samtools mapDamage seqtk
+mkdir $PWD/valid_${TAXID}
+printf "\n"
+
+echo "Grepping NCBI accession IDs for given TaxID = $TAXID"
+grep -w $TAXID /proj/snic2018-8-150/uppstore2018095/private/NBIS_Demo/BOWTIE2_Full_NT_Database/20210127-003704_nt/seqid2taxid.map | cut -f1 > $PWD/valid_${TAXID}/${TAXID}.seq.ids
+
+echo "Using NCBI accession IDs in order to grep reads from given SAM-file"
+grep -wFf $PWD/valid_${TAXID}/${TAXID}.seq.ids $SAM > $PWD/valid_${TAXID}/${TAXID}.sam
+
+echo "Select only reads with MAPQ >= 1 shorter than 100nt and make BAM-file"
+awk 'length($10)<100' $PWD/valid_${TAXID}/${TAXID}.sam > $PWD/valid_${TAXID}/${TAXID}.pruned.sam
+samtools view -bS $PWD/valid_${TAXID}/${TAXID}.pruned.sam -q 1 -h > $PWD/valid_${TAXID}/${TAXID}.bam
+
+echo "Running PMDtools on BAM-file for given TaxID"
+samtools view $PWD/valid_${TAXID}/${TAXID}.bam | python2 /crex/proj/uppstore2018095/private/NBIS_Demo/workshop/PMDtools/pmdtools.0.60.py --platypus --requirebaseq 30 --requiremapq 1 --maxlength 100 --minlength 30 --number 100000 > $PWD/valid_${TAXID}/PMD_temp.txt
+cd $PWD/valid_${TAXID}
+R CMD BATCH /crex/proj/uppstore2018095/private/NBIS_Demo/workshop/PMDtools/plotPMD.v2.R
+cd ..
+
+if [ "$(samtools view $PWD/valid_${TAXID}/${TAXID}.bam | wc -l)" -lt "100" ]; 
+then
+    echo "There are fewer than 100 reads with MAPQ >= 20 and shorter than 100 nt"
+    exit
+fi
+
+echo "Extracting reference fasta-sequences for NCBI accession IDs"
+seqtk subseq /proj/snic2018-8-150/uppstore2018095/private/NBIS_Demo/BOWTIE2_Full_NT_Database/20210127-003704_nt/library/nt/library.fna $PWD/valid_${TAXID}/${TAXID}.seq.ids > $PWD/valid_${TAXID}/${TAXID}.fasta
+
+echo "Running mapDamage on BAM-file for given TaxID = $TAXID"
+mapDamage -i $PWD/valid_${TAXID}/${TAXID}.bam -r $PWD/valid_${TAXID}/${TAXID}.fasta --merge-reference-sequences --no-stats -d $PWD/valid_${TAXID}/results_${TAXID}
+
+printf "\n"
---
+++ authentic_bowtie_screen.R
@@ -0,0 +1,31 @@
+#run this script as: for i in $(cat Swab_torun.txt); do echo ${i}; cd /proj/snic2022-6-60/private/Swab/${i}; Rscript /proj/snic2022-6-60/private/ancient_bowtie_screen.R; done
+
+
+path<-getwd()
+my_dirs<-list.dirs()[grepl("valid*",list.dirs())]
+my_dirs<-gsub("./","",my_dirs)
+my_taxa<-gsub("valid_","",my_dirs)
+
+if(length(my_dirs)==0){q("no")}
+
+CT_CpG_5_vector<-vector()
+GA_CpG_3_vector<-vector()
+for(i in 1:length(my_dirs))
+{
+if(file.exists(paste0(path,"/",my_dirs[i],"/PMD_temp.txt")) & file.info(paste0(path,"/",my_dirs[i],"/PMD_temp.txt"))$size!=0)
+{
+df<-read.delim(paste0(path,"/",my_dirs[i],"/PMD_temp.txt"),header=TRUE,sep="\t")
+CT_CpG_5_vector<-append(CT_CpG_5_vector,df$CT_CpG_5[1])
+GA_CpG_3_vector<-append(GA_CpG_3_vector,df$GA_CpG_3[1])
+}
+else
+{
+CT_CpG_5_vector<-append(CT_CpG_5_vector,NA)
+GA_CpG_3_vector<-append(GA_CpG_3_vector,NA)
+}
+}
+report<-data.frame(TaxID=my_taxa,CT_CpG_5=CT_CpG_5_vector,GA_CpG_3=GA_CpG_3_vector)
+report$Status<-ifelse(report$CT_CpG_5>=0.05 & report$GA_CpG_3>=0.05,"YES","NO")
+print(report)
+
+
